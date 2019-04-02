06fea1b671a0c8aac6f7db9039e8e03c33ff7e24
Pallav Shah <shahpll@schaeffler.com>
2019-04-02 16:52:14 -0400

    Version 6

--- Scripts/displayFeedback.js
+++ Scripts/displayFeedback.js
@@ -1,13 +1,25 @@
-﻿var testApp = angular.module("mymodule10", []).controller("myController10", function ($scope, $http, $window) {
+﻿var testApp = angular.module("mymodule10", []).controller("myController10", function ($scope, $http, $window, $timeout) {
+
+    
 
     $scope.name = localStorage.getItem("key");
-   
+
     if ($scope.name == undefined)
         $window.location.href = 'login.html';
 
     $http.get("WebService1.asmx/GetFeedBackDetailNew").
         then(function (response) {
-        $scope.WebService1class = response.data;
-       
-    });
+            $scope.WebService1class = response.data;
+
+        });
+
+    //$timeout(callAtTimeout, 3*1000);
+    //function callAtTimeout() {
+    //    alert("Timeout occurred");
+    //    $window.location.href = 'logout.html';
+    //}
+
+   
 });
+
+
